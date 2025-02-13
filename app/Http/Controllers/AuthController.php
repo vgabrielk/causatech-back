5e89f154b9afb399776aa1b471667d455e79767c
vgabrielk <vfelix1553@gmail.com>
2025-02-13 02:48:09 -0300

    config: Configura auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,29 +8,20 @@ use Illuminate\Support\Facades\Validator;
 class AuthController{
 
 
-    public function login(Request $request){
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required|string|min:8',
-        ]);
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->accessToken;
+
+            return response()->json([
+                'token' => $token
+            ]);
         }
 
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'Credenciais inválidas'], 401);
-        }
-
-        $token = $user->createToken('auth_token')->accessToken;
-
-        return response()->json([
-            'user' => $user,
-            'token' => $token,
-            'token_type' => 'Bearer'
-        ], 200);
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 
 }
